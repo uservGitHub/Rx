a07d40f448d773a0aca8a7edb154e7cc47de4246
uservGitHub <userv@126.com>
2018-03-21 14:49:22 +0800

    Excercise2

--- csharp/RxHOL/Excercise/ExcerBaseLibrary/TickTimer.cs
+++ csharp/RxHOL/Excercise/ExcerBaseLibrary/TickTimer.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Diagnostics;
 
 namespace ExcerBaseLibrary
 {
     public static class DelayTimer
     {
         private static Timer timer;
+        private static int SPAN = 11;   //11毫秒误差
+        private static long Begin = 0;
+        private static int sumMs = 0;
+        private static Stopwatch watch;
         private static readonly object delayLock = new object();
         private static Tuple<Action, int> currentTask = null;
         private static Queue<Tuple<Action, int>> delayList = new Queue<Tuple<Action, int>>();
@@ -28,6 +33,10 @@ namespace ExcerBaseLibrary
                     else
                     {
                         currentTask.Item1.Invoke();
+
+                        if(Begin == 0)
+                            Begin = watch.ElapsedMilliseconds;
+                        
                         lock (delayLock)
                         {
                             delayList.Dequeue();
@@ -35,23 +44,22 @@ namespace ExcerBaseLibrary
                         Next();
                     }
                 }));
+                
+                watch = new Stopwatch();
             }
             if(delayList.Count == 1)
             {
+                sumMs = 0;
                 timer.Change(0, 0);
-            }            
-        }
-        /// <summary>
-        /// 假设定时器没有开启
-        /// 动作：开启并清空
-        /// </summary>
-        private static void AutoStart()
-        {
-            if (timer != null && currentTask == null)
-            {
-                timer.Change(0, 0);
+                //开始计时
+                if (!watch.IsRunning)
+                {
+                    watch.Restart();
+                    Console.WriteLine("Restart");
+                }
             }
         }
+
         private static bool Next()
         {
             lock (delayLock)
@@ -59,11 +67,19 @@ namespace ExcerBaseLibrary
                 if (delayList.Count > 0)
                 {
                     currentTask = delayList.Peek();
-                    timer.Change(currentTask.Item2, 0);
+                    //计算总时间，并调整累计时间
+                    sumMs += currentTask.Item2;
+                    timer.Change(sumMs - watch.ElapsedMilliseconds-SPAN, 0);
+
                     return true;
                 }else
                 {
                     timer.Change(Timeout.Infinite, Timeout.Infinite);
+                    currentTask = null;
+                    watch.Stop();
+                    timer.Dispose();
+                    timer = null;
+                    Console.WriteLine("Dispose");
                 }
                 return false;
             }
--- csharp/RxHOL/Excercise/Excercise02/Program.cs
+++ csharp/RxHOL/Excercise/Excercise02/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Diagnostics;
+using System.Reactive.Linq;
 
 using ExcerBaseLibrary;
 
@@ -12,22 +13,125 @@ namespace Excercise02
     {
         static void Main(string[] args)
         {
+            var sources = GetSourcesFromExcercise2();
+            var clock = new Stopwatch();
+            clock.Restart();
+            OnSubscribeToNEC(sources, clock, true);
+
+            
+        }
+
+        /// <summary>
+        /// Observable.Function(立即对象或异步执行返回)
+        /// </summary>
+        /// <returns></returns>
+        static IList<Tuple<string, IObservable<int>>> GetSourcesFromExcercise2()
+        {
+            var list = new List<Tuple<string, IObservable<int>>>();
+            list.Add(new Tuple<string, IObservable<int>>(
+                "Never<int>()",
+                Observable.Never<int>()
+                ));
+            list.Add(new Tuple<string, IObservable<int>>(
+                "Empty<int>()",
+                Observable.Empty<int>()
+                ));
+            list.Add(new Tuple<string, IObservable<int>>(
+                "Throw<int>(new Exception(\"Oops\"))",
+                Observable.Throw<int>(new Exception("Oops"))
+                ));
+            list.Add(new Tuple<string, IObservable<int>>(
+                "Return(77)",
+                Observable.Return<int>(77)
+                ));
+            list.Add(new Tuple<string, IObservable<int>>(
+                "Range(10,3)",
+                Observable.Range(10, 3)
+                ));
+            list.Add(new Tuple<string, IObservable<int>>(
+                "Generate(0,i=>i<3,i=>i+1,i=>i*i)",
+                Observable.Generate(0, i => i < 3, i => i + 1, i => i * i)
+                ));
+            //list.Add(new Tuple<string, IObservable<int>>(
+            //    "Return(100,耗时100毫秒)",
+            //    Observable.Return<int>(100)
+            //    ));
+
+            return list;
+        }
+
+        /// <summary>
+        /// 对数据源顺序观察其执行情况：
+        /// [=====> No.{序号/总数:D2}\t{主题}\t{源的数量}
+        /// 方法:  {内容}\t{总运行时间}
+        /// ...
+        /// <======]\t{总运行时间}\n
+        /// 按q跳出数据源的观察
+        /// </summary>
+        /// <param name="sources"></param>
+        /// <param name="Watch"></param>
+        static void OnSubscribeToNEC(IEnumerable<Tuple<string, IObservable<int>>> sources, Stopwatch Watch, bool resetWatch=false)
+        {
+            //No.序号 主题
+            const string tagFmt = "[=====> No.{0:D2}/{1:D2}\t{2}\t{3}";
+            int i = 1;
+            int count = sources.Count();
+
+            Console.WriteLine(string.Format("==>遍历{0}个发射端:\n", count));
+            foreach (var group in sources)
+            {
+                if (resetWatch) Watch.Restart();
+                Console.WriteLine(tagFmt, i++, count, group.Item1, Watch.ElapsedMilliseconds);
+
+                IDisposable subscription = group.Item2.Subscribe(
+                x => Console.WriteLine("OnNext:  {0}\t{1}", x, Watch.ElapsedMilliseconds),
+                ex => Console.WriteLine("OnError: {0}\t{1}", ex.Message, Watch.ElapsedMilliseconds),
+                () => Console.WriteLine("OnCompleted\t{0}", Watch.ElapsedMilliseconds)
+            );
+                Console.WriteLine("<======]\t{0}\n", Watch.ElapsedMilliseconds);
+
+                #region q跳出
+                //Console.WriteLine("Press ENTER to unsubscribe...");
+                //Console.ReadKey();
+                var line = Console.In.ReadLine();
+                if (line.Length == 1 && line.ToLower() == "q")
+                {
+                    break;
+                }
+                #endregion
+
+                subscription.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// DelayTimer的使用：
+        /// 使用绝对时序顺序执行添加的任务
+        /// </summary>
+        static void DelayTimer_Sample()
+        {
             Stopwatch watch = new Stopwatch();
             watch.Start();
             Console.WriteLine(watch.ElapsedMilliseconds);
-            DelayTimer.Add(() =>
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(watch.ElapsedMilliseconds);
-            }, 1500);
-            DelayTimer.Add(() =>
+                DelayTimer.Add(() =>
+                {
+                    Console.WriteLine(watch.ElapsedMilliseconds);
+                }, 1000);
+                System.Threading.Thread.Sleep(850);
+            }
+
+            System.Threading.Thread.Sleep(2000);
+
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(watch.ElapsedMilliseconds);
-            }, 1500);
-            DelayTimer.Add(() =>
-            {
-                Console.WriteLine(watch.ElapsedMilliseconds);
-            }, 1500);
-            Console.ReadKey();
+                DelayTimer.Add(() =>
+                {
+                    Console.WriteLine(watch.ElapsedMilliseconds);
+                }, 1000);
+            }
+            //Console.ReadKey();
         }
     }
 }
